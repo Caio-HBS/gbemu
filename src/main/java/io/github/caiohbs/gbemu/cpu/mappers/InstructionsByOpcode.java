6a1980b227bea20cae5e1c3b942aad61d364e165
Caio Bianchi <caiohbs23@gmail.com>
2026-08-04 13:28:36 -0300

    Extend CPU instruction set with opcode mappings, addressing modes, and condition checks. Add instruction execution logic and flag handling.

--- src/main/java/io/github/caiohbs/gbemu/cpu/mappers/InstructionsByOpcode.java
+++ src/main/java/io/github/caiohbs/gbemu/cpu/mappers/InstructionsByOpcode.java
@@ -2,6 +2,7 @@ package io.github.caiohbs.gbemu.cpu.mappers;
 
 import io.github.caiohbs.gbemu.cpu.Instruction;
 
+import static io.github.caiohbs.gbemu.cpu.enums.ConditionType.*;
 import static io.github.caiohbs.gbemu.cpu.enums.InstructionType.*;
 import static io.github.caiohbs.gbemu.cpu.enums.AddressMode.*;
 import static io.github.caiohbs.gbemu.cpu.enums.RegisterType.*;
@@ -11,12 +12,13 @@ import java.util.Map;
 public final class InstructionsByOpcode {
 
     public static final Map<Integer, Instruction> INSTRUCTIONS = Map.<Integer, Instruction>ofEntries(
-            Map.entry(0x00, new Instruction(IN_NOP, AM_IMP, null, null, null, 0)),
-            Map.entry(0x05, new Instruction(IN_DEC, AM_R, RT_B, null, null, 0)),
-            Map.entry(0x0E, new Instruction(IN_LD, AM_R_D8, RT_C, null, null, 0)),
-            Map.entry(0xAF, new Instruction(IN_XOR, AM_R, RT_A, null, null, 0)),
-            Map.entry(0xC3, new Instruction(IN_JP, AM_D16, null, null, null, 0)),
-            Map.entry(0xF3, new Instruction(IN_DI, null, null, null, null, 0))
+            Map.entry(0x00, new Instruction(IN_NOP, AM_IMP,  RT_NONE, RT_NONE, CT_NONE, 0)),
+            Map.entry(0x05, new Instruction(IN_DEC, AM_R,    RT_B,    RT_NONE, CT_NONE, 0)),
+            Map.entry(0x0E, new Instruction(IN_LD,  AM_R_D8, RT_C,    RT_NONE, CT_NONE, 0)),
+            Map.entry(0xAF, new Instruction(IN_XOR, AM_R,    RT_A,    RT_NONE, CT_NONE, 0)),
+            Map.entry(0x21, new Instruction(IN_LD,  AM_D16,  RT_HL,   RT_NONE, CT_NONE, 0)),
+            Map.entry(0xC3, new Instruction(IN_JP,  AM_D16,  RT_NONE, RT_NONE, CT_NONE, 0)),
+            Map.entry(0xF3, new Instruction(IN_DI,  AM_NONE, RT_NONE, RT_NONE, CT_NONE, 0))
     );
 
     public static Instruction get(int opcode) {
